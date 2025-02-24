f6ea976866a2b6fe33ffca5bae4830919947402b
Sacha Escudier <sacha.escudier@princeton.edu>
2025-02-24 12:17:39 -0500

    Add frame_extraction script

---
+++ video_frame_extraction.py
@@ -0,0 +1,5 @@
+from frames import extract_frames
+
+input_directory = "C:/Users/escud/Desktop/Marangoni/Videos/0225-Experiments/H=4cm.MOV"
+output_directory = "C:/Users/escud/Desktop/Marangoni/Videos/0225-Experiments/H=4cm_frames"
+extract_frames(input_directory, output_directory)
