9c7ec9a527a0e01f3b895eabf3f6f77d81766cde
Sacha Escudier <sacha.escudier@princeton.edu>
2025-03-07 13:44:11 -0500

    adapt code for particles leaving the frame (supposedly more robust)

--- video_frame_extraction.py
+++ video_frame_extraction.py
@@ -1,5 +1,5 @@
 from frames import extract_frames
 
-input_directory = "C:/Users/escud/Desktop/Marangoni/Videos/0225-Experiments/H=4cm.MOV"
-output_directory = "C:/Users/escud/Desktop/Marangoni/Videos/0225-Experiments/H=4cm_frames"
+input_directory = "C:/Users/escud/Desktop/Marangoni/Videos/0306-Experiments/H=1cm.MOV"
+output_directory = "C:/Users/escud/Desktop/Marangoni/Videos/0306-Experiments/H=1cm_frames"
 extract_frames(input_directory, output_directory, grayscale=True)
